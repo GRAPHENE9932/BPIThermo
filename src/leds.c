adc8170447a1c47a99ec0358e7728849c5ba62be
Artem Klimiński <graphene64@gmail.com>
2025-04-22 23:00:08 +0200

    feat: Implement the 7-segment LED control

---
+++ src/leds.c
@@ -0,0 +1,129 @@
+#include "leds.h"
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Define pins for the TLC6C598 shift register's inputs.
+#define PORT_SER_IN_SRCK PORTB
+#define DDR_SER_IN_SRCK DDRB
+#define BIT_SER_IN 0
+#define BIT_SRCK 1
+#define PORT_RCK_CLR_G PORTD
+#define DDR_RCK_CLR_G DDRD
+#define BIT_RCK 7
+#define BIT_CLR 6
+#define BIT_G 5
+
+// Define the duration of a half of a clock cycle for shift register control in microseconds.
+// 10 us corresponds to 50 kHz.
+#define HALF_CYCLE_US 10
+
+uint8_t leds[6];
+
+// Considering only delays, this operation lasts for 17*HALF_CYCLE_US.
+static void send_byte_to_shift_register(uint8_t byte) {
+    for (uint8_t i = 0; i < 8; i++) {
+        uint8_t cur_bit = byte & 0b10000000;
+
+        if (cur_bit) {
+            PORT_SER_IN_SRCK |= (1 << BIT_SER_IN);
+        }
+        else {
+            PORT_SER_IN_SRCK &= ~(1 << BIT_SER_IN);
+        }
+
+        _delay_us(HALF_CYCLE_US);
+
+        PORT_SER_IN_SRCK |= (1 << BIT_SRCK);
+
+        _delay_us(HALF_CYCLE_US);
+
+        PORT_SER_IN_SRCK &= ~(1 << BIT_SRCK);
+
+        byte <<= 1;
+    }
+
+    PORT_RCK_CLR_G |= (1 << BIT_RCK);
+    _delay_us(HALF_CYCLE_US);
+    PORT_RCK_CLR_G &= ~(1 << BIT_RCK);
+}
+
+void leds_init(void) {
+    // PD0, PD1, PD2, PD3, PD4, PA2 are controlling P-type enhancement MOSFET
+    // gates for the least significant digits respectively.
+    DDRD |= (1 << PD0) | (1 << PD1) | (1 << PD2) | (1 << PD3) | (1 << PD4);
+    PORTD |= (1 << PD0) | (1 << PD1) | (1 << PD2) | (1 << PD3) | (1 << PD4);
+    DDRA |= (1 << PA2);
+    PORTA |= (1 << PA2);
+
+    // Initialize pins controlling the TLC6C598 shift register.
+    DDR_SER_IN_SRCK |= (1 << BIT_SER_IN) | (1 << BIT_SRCK);
+    PORT_SER_IN_SRCK &= ~((1 << BIT_SER_IN) | (1 << BIT_SRCK));
+    DDR_RCK_CLR_G |= (1 << BIT_RCK) | (1 << BIT_CLR) | (1 << BIT_G);
+    PORT_RCK_CLR_G |= (1 << BIT_G);
+    PORT_RCK_CLR_G &= ~((1 << BIT_RCK) | (1 << BIT_CLR));
+    // We just set the CLR bit to 0 to reset it. Now wait half a cycle and return it to the normal state: 1.
+    _delay_us(HALF_CYCLE_US);
+    PORT_RCK_CLR_G |= (1 << BIT_CLR);
+
+    send_byte_to_shift_register(0b00000000);
+
+    for (uint8_t i = 0; i < 6; i++) {
+        leds[i] = 0;
+    }
+}
+
+void leds_flash_once(void) {
+    for (uint8_t i = 0; i < 6; i++) {
+        send_byte_to_shift_register(leds[i]);
+
+        switch (i) {
+        case 0:
+            PORTD &= ~(1 << PD0);
+            break;
+        case 1:
+            PORTD &= ~(1 << PD1);
+            break;
+        case 2:
+            PORTD &= ~(1 << PD2);
+            break;
+        case 3:
+            PORTD &= ~(1 << PD3);
+            break;
+        case 4:
+            PORTD &= ~(1 << PD4);
+            break;
+        case 5:
+            PORTA &= ~(1 << PA2);
+            break;
+        }
+
+        PORT_RCK_CLR_G &= ~(1 << BIT_G);
+
+        _delay_us(HALF_CYCLE_US);
+
+        PORT_RCK_CLR_G |= (1 << BIT_G);
+
+        switch (i) {
+        case 0:
+            PORTD |= (1 << PD0);
+            break;
+        case 1:
+            PORTD |= (1 << PD1);
+            break;
+        case 2:
+            PORTD |= (1 << PD2);
+            break;
+        case 3:
+            PORTD |= (1 << PD3);
+            break;
+        case 4:
+            PORTD |= (1 << PD4);
+            break;
+        case 5:
+            PORTA |= (1 << PA2);
+            break;
+        }
+    }
+}
---
+++ src/leds.h
@@ -0,0 +1,19 @@
+#ifndef LEDS_H
+#define LEDS_H
+
+#include <stdint.h>
+
+// Buffer that stores the desired state of 7-segment display's LEDs.
+// Each byte is responsible for its digit on 7-segment display.
+// 0th byte is responsible for the least significant blue digit.
+// 1st byte for the most significant blue digit.
+// 2nd byte for the least significant red digit.
+// And so on.
+// In the each byte, its bits are responsible for the corresponding LED segments (starting from LSB):
+// A B C D E F G DP
+extern uint8_t leds[6];
+
+void leds_init(void);
+void leds_flash_once(void);
+
+#endif // LEDS_H
