dabc4e4581dbf6bd83c57e0892adf265c5320cc5
Artem Klimiński <graphene64@gmail.com>
2025-04-25 22:24:26 +0200

    feat: Show temperature on the LED display

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
+#include "hdc2080.h"
+#include "leds.h"
+
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/power.h>
 #include <stdbool.h>
-#include "hdc2080.h"
+
+static const uint8_t SEVEN_SEGMENT_DIGITS[10] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01100111, // 9
+};
+
+#define SEVEN_SEGMENT_MINUS 0b01000000
+#define SEVEN_SEGMENT_DP 0b10000000
 
 static void debug_shiftout(uint8_t byte) {
     bool cur_bit = byte & 0b10000000 ? true : false;
@@ -27,18 +45,81 @@ static void debug_shiftout(uint8_t byte) {
     }
 }
 
+static void put_temperature_on_leds(fixed16 temperature) {
+    bool is_negative = false;
+    if (temperature < 0) {
+        temperature = -temperature;
+        is_negative = true;
+    }
+
+    uint8_t digits[5]; // [4] [3] [2].[1] [0]
+
+    uint8_t integer_part = temperature >> 8;
+
+    digits[4] = integer_part / 100;
+    digits[3] = integer_part / 10 - digits[4] * 100;
+    digits[2] = integer_part % 10;
+
+    uint8_t fractional_part = temperature;
+
+    digits[1] = ((uint16_t)fractional_part * 10) >> 8;
+    digits[0] = (((uint16_t)fractional_part * 100) >> 8) - digits[1] * 10;
+
+    // Assigment of the LED values could be done in a loop, but there are so few iterations
+    // and so many conditions, it would be much more comprehensible without a loop.
+    if (is_negative) {
+        leds[5] = SEVEN_SEGMENT_MINUS;
+
+        // In the negative range we assume that digit[4] is always 0,
+        // because neither in Celsius or Fahrenheit the temperature can be lower than -40.
+        if (digits[3] == 0) {
+            leds[4] = SEVEN_SEGMENT_DIGITS[digits[2]] | SEVEN_SEGMENT_DP;
+            leds[3] = SEVEN_SEGMENT_DIGITS[digits[1]];
+            leds[2] = SEVEN_SEGMENT_DIGITS[digits[0]];
+        }
+        else {
+            leds[4] = SEVEN_SEGMENT_DIGITS[digits[3]];
+            leds[3] = SEVEN_SEGMENT_DIGITS[digits[2]] | SEVEN_SEGMENT_DP;
+            leds[2] = SEVEN_SEGMENT_DIGITS[digits[1]];
+        }
+    }
+    else {
+        if (digits[3] == 0) {
+            leds[5] = 0b00000000;
+            leds[4] = SEVEN_SEGMENT_DIGITS[digits[2]] | SEVEN_SEGMENT_DP;
+            leds[3] = SEVEN_SEGMENT_DIGITS[digits[1]];
+            leds[2] = SEVEN_SEGMENT_DIGITS[digits[0]];
+        }
+        else if (digits[4] == 0) {
+            leds[5] = SEVEN_SEGMENT_DIGITS[digits[3]];
+            leds[4] = SEVEN_SEGMENT_DIGITS[digits[2]] | SEVEN_SEGMENT_DP;
+            leds[3] = SEVEN_SEGMENT_DIGITS[digits[1]];
+            leds[2] = SEVEN_SEGMENT_DIGITS[digits[0]];
+        }
+        else {
+            leds[5] = SEVEN_SEGMENT_DIGITS[digits[4]];
+            leds[4] = SEVEN_SEGMENT_DIGITS[digits[3]];
+            leds[3] = SEVEN_SEGMENT_DIGITS[digits[2]] | SEVEN_SEGMENT_DP;
+            leds[2] = SEVEN_SEGMENT_DIGITS[digits[1]];
+        }
+    }
+}
+
 int main(void) {
     clock_prescale_set(clock_div_1); // Disable the default /8 prescaler.
 
     DDRB |= (1 << PB7);
 
+    leds_init();
+    
+
     hdc2080_init();
     while (1) {
-        fixed16 celsius = hdc2080_measure_temperature_sync();
-        debug_shiftout(celsius >> 8);
-        debug_shiftout(celsius);
-
-        _delay_ms(5000);
+        hdc2080_start_measurement();
+        while (!hdc2080_is_measurement_over());
+        fixed16 temperature = hdc2080_get_temperature_celsius();
+        put_temperature_on_leds(temperature);
+        leds_flash_once();
     }
 
     while (1) {
